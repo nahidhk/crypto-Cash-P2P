15ff946abb46aa97cb899b292cfe68026f2c8308
nahidhk <nahidappltd@gmail.com>
2024-03-21 10:25:06 +0600

    today's update

--- app.js
+++ app.js
@@ -3,3 +3,48 @@ function bodyai(){
   document.getElementById("navimg").classList="img-box myani";
  
 }
+function vdload(){
+  document.getElementById("hello").innerHTML="hello"
+}
+
+
+
+
+
+
+
+window.addEventListener('load', () => {
+  let deferredPrompt;
+
+
+  const addToHomeBtn = document.getElementById('add-to-home-btn');
+
+  
+  addToHomeBtn.addEventListener('click', () => {
+    
+    if (deferredPrompt) {
+      deferredPrompt.prompt();
+
+      
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt');
+        } else {
+          console.log('User dismissed the A2HS prompt');
+        }
+     
+        deferredPrompt = null;
+      });
+    }
+  });
+
+ 
+  window.addEventListener('beforeinstallprompt', (event) => {
+    
+    event.preventDefault();
+   
+    deferredPrompt = event;
+    
+    addToHomeBtn.style.display = 'block';
+  });
+});
