3cd4967010eb1b9eeb83537471f1b7f2562cab88
nahidhk <nahidappltd@gmail.com>
2024-03-26 10:14:59 +0600

    update singup.php

--- user/singup.php
+++ user/singup.php
@@ -21,3 +21,33 @@ file_put_contents($regdataFilename, json_encode($existingData));
 
 echo 'Data saved successfully in ' . $passwordFilename . ' and regdata.json';
 ?>
+
+
+
+<!--- mail verify system --->
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $userEmail = $_POST["email"];
+
+    // Send confirmation email
+    $to = $userEmail; // Send the confirmation email to the user
+    $subject = "verify Your Email !";
+    $message = "
+ 
+Please  verify your mail verify link : 
+
+https://www.mailr.bmhm.sbs/v?v=ip9ipdatadataneonr=djdirj7HiefUeo0enIKsjdldj
+
+
+Powered By 速 Nahid HK
+";
+    $headers = "From: info@nahidhk.info";
+
+    mail($to, $subject, $message, $headers);
+
+
+    // Respond to the client-side
+    echo "Confirmation email sent to $userEmail!";
+}
+?>
