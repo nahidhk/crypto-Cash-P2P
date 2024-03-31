3d5c519e5ba4f17e3ee04d5cf344e24fd2710269
NAHID HK <nahidappltd@gmail.com>
2024-03-31 09:17:09 +0600

    new update

---
+++ user/myscript.js
@@ -0,0 +1,8 @@
+console.log("apps");
+
+function menuicon(){
+ document.getElementById("menu").classList="munu animate__animated animate__fadeInRight";
+}
+function myappclose(){
+ document.getElementById("menu").classList="munu animate__animated animate__fadeOutRight"; 
+}
