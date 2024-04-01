e6d02f76759cf6e85ce32381b6803e9c35478cde
NAHID HK <nahidappltd@gmail.com>
2024-04-01 22:05:06 +0600

    js add

--- user/myscript.js
+++ user/myscript.js
@@ -6,3 +6,15 @@ function menuicon(){
 function myappclose(){
  document.getElementById("menu").classList="munu animate__animated animate__fadeOutRight"; 
 }
+function local1(){
+document.getElementById("buyprice").innerText="125"
+}
+local1();
+function sell(){
+    document.getElementById("sell").style.display="block";
+    document.getElementById("buy").style.display="none";
+}
+function buy(){
+    document.getElementById("sell").style.display="none";
+    document.getElementById("buy").style.display="block";
+}
