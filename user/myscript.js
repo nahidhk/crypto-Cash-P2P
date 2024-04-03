9a0bdfbf62bfdbfe2e3ad5624dba110ac005a266
NAHID HK <nahidappltd@gmail.com>
2024-04-03 08:22:57 +0600

    new update

--- user/myscript.js
+++ user/myscript.js
@@ -1,5 +1,5 @@
 console.log("apps");
-
+let price = JSON.parse('{"sell":"120","buy":"125"}');
 function menuicon(){
  document.getElementById("menu").classList="munu animate__animated animate__fadeInRight";
 }
@@ -7,7 +7,7 @@ function myappclose(){
  document.getElementById("menu").classList="munu animate__animated animate__fadeOutRight"; 
 }
 function local1(){
-document.getElementById("buyprice").innerText="125"
+document.getElementById("buyprice").innerText=price.buy;
 }
 local1();
 function sell(){
@@ -18,3 +18,7 @@ function buy(){
     document.getElementById("sell").style.display="none";
     document.getElementById("buy").style.display="block";
 }
+function sellprice(){
+    document.getElementById("sellprice").innerText=price.sell;
+}
+sellprice();
